4f439a7def94478f906b56efd98ad2e29a8b1fb1
jkanangila <jakanangila@gmail.com>
2021-11-11 20:36:39 +0100

    Added: (1) post detail screen; (2) list of screens

--- app/src/main/java/com/tutorial/socialnetworkapp/presentation/util/Screen.kt
+++ app/src/main/java/com/tutorial/socialnetworkapp/presentation/util/Screen.kt
@@ -7,5 +7,11 @@ sealed class Screen(val route: String){
     object MainFeedScreen: Screen("main_feed_screen")
     object PostDetailScreen: Screen("post_detail_screen")
     object ChatScreen: Screen("chat_screen")
+    object MessageScreen: Screen("message_screen")
+    object ProfileScreen: Screen("profile_screen")
+    object EditProfileScreen: Screen("edit_profile_screen")
+    object PersonListScreen: Screen("person_list_screen")
+    object CreatePostScreen: Screen("create_post_screen")
+    object ActivityScreen: Screen("activity_screen")
     object SearchScreen: Screen("search_screen")
 }
